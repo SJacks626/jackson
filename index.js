f887073bd1596f842e8da727734804c66a6d37ff
SJacks626 <114336230+SJacks626@users.noreply.github.com>
2022-10-13 23:15:05 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,89 @@
+// create a variable called speedLimit and another called mySpeed
+// set their values to numbers between 1 and 100
+let speedLimit = 65
+let mySpeed = 70
+
+if (mySpeed>speedLimit){
+    console.log("Slow Down! Mom is mad!")
+}else if (mySpeed = speedLimit){
+console.log( "Everyone is happy!")
+} else {
+    ("Speed up! Dad is mad!")
+}
+
+
+
+// using a conditional, determine if mySpeed is greater than the speedLimit
+// if true, print "Slow Down! Mom is mad!" to the console
+// if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+// if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+
+
+
+
+// create two variables, one named alarmSet, the other openDoor
+// set them to a boolean value
+let alarmSet = true
+let openDoor = false
+
+if (alarmSet && openDoor) {
+    console.log("Everything is fine.")
+} else {
+    console.log ('Sound Alarm!');
+}
+
+
+// using a conditional, determine if alarm is set. 
+// if alarm is set and door is set to open, print "Sound Alarm!" to the console
+// otherwise, print "Everything is fine." to the console.
+
+
+
+
+// create two variables, username and password
+// create a conditional, if the username is "Tommy123" and the password is "12345"
+// or the username is "Timmy456" and the password is "6789", print "Admin Login Successful" to the console
+// otherwise, print "Admin Access Denied"
+
+
+
+
+// write code that will set the value of studentClass based on studentGrade
+// studentGrade will be K-12
+// K-6 will be Elementary
+// 7-8 will be Middle
+// 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
+// Anything other than these values will return "Error" to the console
+
+
+
+
+
+// write a for loop that will iterate backwards from 10 to -10
+for(let i=10; i>= -10; i--)
+console.log(i);
+
+
+
+// write a do/while loop that prints 1 through 50
+
+
+// edit the previous do/while loop so that it prints the remainder when the loop number is divided by 4
+
+
+ Someone messed up the following for loop
+// fix the following infinite loop, uncomment to test
+ 
+    for(let i = 11; i <= 10; i++){
+        console.log(i);
+    } 
+     for(let i = 11; i <= 11; i++){
+        console.log(i)
+ }
+*/
+
+
+
+
+
+
